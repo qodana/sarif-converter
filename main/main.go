05f8f8d30df4854e22ab9ac4c05a6686d5c5d45e
Tomo Masakura <tomo.masakura@gmail.com>
2022-10-07 13:28:04 +0900

    fix don't stop with convert error

--- main/main.go
+++ main/main.go
@@ -7,19 +7,33 @@ import (
 )
 
 func main() {
+	input := tryRead()
+
+	output := tryConvert(input)
+
+	tryWrite(output)
+}
+
+func tryRead() []byte {
 	input, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	return input
+}
 
-	output, _ := converter.Convert(input)
+func tryConvert(input []byte) []byte {
+	output, err := converter.Convert(input)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	return output
+}
 
-	err = os.WriteFile(os.Args[2], output, 0666)
+func tryWrite(output []byte) {
+	err := os.WriteFile(os.Args[2], output, 0666)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
