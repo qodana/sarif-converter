68c3e665d28e5fd50bf6277cabddf0c4d6bcdb63
Tomo Masakura <tomo.masakura@gmail.com>
2023-08-31 15:53:58 +0900

    sast report analyzer information to be taken from metadata
    * https://gitlab.com/ignis-build/sarif-converter/-/merge_requests/43#note_1536632192

--- sast/analyzer/analyzer.go
+++ sast/analyzer/analyzer.go
@@ -14,8 +14,8 @@ func NewAnalyzer() Analyzer {
 
 func (a Analyzer) ToSast(metadata meta.Metadata) report.AnalyzerDetails {
 	return report.AnalyzerDetails{
-		ID:      "sarif-converter",
-		Name:    "SARIF Converter",
+		ID:      metadata.Package,
+		Name:    metadata.Name,
 		Version: metadata.SemanticVersion(),
 		Vendor:  report.Vendor{Name: "SARIF Converter"},
 	}
